023dea709e6c16829173044fa5e4e9bfebf89b47
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-13 16:42:12 +0300

    refactor: remove NextStateEnum from exports

--- pythonium/engine/enums/__init__.py
+++ pythonium/engine/enums/__init__.py
@@ -1,5 +1,4 @@
 from .direction import Direction
-from .next_state import NextStateEnum
 from .states import State
 
-__all__ = ("Direction", "NextStateEnum", "State")
+__all__ = ("Direction", "State")
