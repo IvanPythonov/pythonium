82b685bba5cf649a62a92f2d9833d2ee007b22f4
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-13 16:42:14 +0300

    feat: implement proper handshake state transition logic

--- pythonium/server/routers/handshake.py
+++ pythonium/server/routers/handshake.py
@@ -1,15 +1,14 @@
 """Some logic for router."""
 
-from pythonium.engine import Router
+from pythonium.engine import Client, Router
+from pythonium.engine.enums import State
 from pythonium.engine.packets import Handshake
 
 router = Router(name=__name__)
 
 
 @router.on(Handshake)
-async def on_handshake(handshake: Handshake) -> None:
-    if None:
-        msg = "none is something"
-        raise ValueError(msg)  # kick with this error
-    print(handshake)
-    # return PingPong(timestamp=time.time())
+async def on_handshake(handshake: Handshake, client: Client) -> None:
+    client.session.state = (
+        State.STATUS if handshake.next_state == 1 else State.PLAY
+    )
