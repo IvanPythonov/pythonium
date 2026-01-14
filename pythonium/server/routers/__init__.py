a4ef532ccc5cb9e5dbdd487660a87af65761c1c6
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-14 22:22:57 +0300

    feat: add configuration and login router exports

--- pythonium/server/routers/__init__.py
+++ pythonium/server/routers/__init__.py
@@ -1,4 +1,11 @@
+from .configuration import router as configuration_router
 from .handshake import router as handshake_router
+from .login import router as login_router
 from .status import router as status_router
 
-__all__ = ("handshake_router", "status_router")
+__all__ = (
+    "configuration_router",
+    "handshake_router",
+    "login_router",
+    "status_router",
+)
