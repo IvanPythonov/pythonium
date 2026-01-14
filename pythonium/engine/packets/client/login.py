5282e092b061e6e6abb49390195896206c5ab14e
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-14 22:22:40 +0300

    feat: add client login packets (LoginStart, LoginAcknowledged, LoginCustomPayload)

---
+++ pythonium/engine/packets/client/login.py
@@ -0,0 +1,38 @@
+from typing import ClassVar
+
+from pythonium.engine.enums import Direction, State
+from pythonium.engine.packets import Packet
+from pythonium.engine.types import UUID, String, VarInt
+
+
+class LoginStart(Packet, kw_only=True):
+    """Packet representing login start."""
+
+    __state__ = State.LOGIN
+    __direction__ = Direction.SERVERBOUND
+
+    packet_id: ClassVar[VarInt] = 0x00
+
+    name: String
+    uuid: UUID
+
+
+class LoginAcknowledged(Packet, kw_only=True):
+    """Packet representing login acknowledged."""
+
+    __state__ = State.LOGIN
+    __direction__ = Direction.SERVERBOUND
+
+    packet_id: ClassVar[VarInt] = 0x03
+
+
+class LoginCustomPayload(Packet, kw_only=True):
+    """Packet representing login custom payload."""
+
+    __state__ = State.LOGIN
+    __direction__ = Direction.SERVERBOUND
+
+    packet_id: ClassVar[VarInt] = 0x02
+
+    brand: String
+    data: String
