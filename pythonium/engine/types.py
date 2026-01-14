4000b61e604676386600dfaac87e54f7de3e0955
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-14 22:22:41 +0300

    refactor: update type definitions and remove unused imports

--- pythonium/engine/types.py
+++ pythonium/engine/types.py
@@ -7,12 +7,29 @@ from pythonium.engine.codecs import (
     FloatCodec,
     IntCodec,
     LongCodec,
+    PositionCodec,
     ShortCodec,
     StringCodec,
     UnsignedByteCodec,
     UnsignedShortCodec,
+    UUIDCodec,
     VarIntCodec,
     VarLongCodec,
+)
+from pythonium.engine.codecs.array import (
+    BooleanArrayCodec,
+    ByteArrayCodec,
+    DoubleArrayCodec,
+    FloatArrayCodec,
+    IntArrayCodec,
+    LongArrayCodec,
+    ShortArrayCodec,
+    StringArrayCodec,
+    UnsignedByteArrayCodec,
+    UnsignedShortArrayCodec,
+    UUIDArrayCodec,
+    VarIntArrayCodec,
+    VarLongArrayCodec,
 )
 
 type VarInt = Annotated[int, VarIntCodec()]
@@ -32,4 +49,29 @@ type Double = Annotated[float, DoubleCodec()]
 
 type String = Annotated[str, StringCodec()]
 
+type UUID = Annotated[str, UUIDCodec()]
+
 type Boolean = Annotated[bool, BooleanCodec()]
+
+type ByteArray = Annotated[list[int], ByteArrayCodec()]
+type UByteArray = Annotated[list[int], UnsignedByteArrayCodec()]
+
+type ShortArray = Annotated[list[int], ShortArrayCodec()]
+type UShortArray = Annotated[list[int], UnsignedShortArrayCodec()]
+
+type IntArray = Annotated[list[int], IntArrayCodec()]
+type LongArray = Annotated[list[int], LongArrayCodec()]
+
+type FloatArray = Annotated[list[float], FloatArrayCodec()]
+type DoubleArray = Annotated[list[float], DoubleArrayCodec()]
+
+type StringArray = Annotated[list[str], StringArrayCodec()]
+
+type UUIDArray = Annotated[list[str], UUIDArrayCodec()]
+
+type BooleanArray = Annotated[list[bool], BooleanArrayCodec()]
+
+type VarIntArray = Annotated[list[int], VarIntArrayCodec()]
+type VarLongArray = Annotated[list[int], VarLongArrayCodec()]
+
+type Position = Annotated[tuple[int, int, int], PositionCodec()]
