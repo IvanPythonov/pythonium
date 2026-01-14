79f77b462c6bd56d29b071c83d79da4ec7beeb50
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-14 22:22:04 +0300

    feat: add ArrayCodec base class with lazy VarInt import

--- pythonium/engine/codecs/base.py
+++ pythonium/engine/codecs/base.py
@@ -1,5 +1,5 @@
 import struct
-from typing import Annotated, get_args, get_origin
+from typing import Annotated, ClassVar, get_args, get_origin
 
 from pythonium.engine.typealiases import Deserialized
 
@@ -12,6 +12,37 @@ class Codec[T]:
 
     def deserialize(self, data: bytes) -> Deserialized[T]:
         raise NotImplementedError
+
+
+class ArrayCodec[T](Codec):
+    """Codec for arrays prefixed with length."""
+
+    __element_codec__: ClassVar[Codec]
+    __length_codec__: ClassVar[Codec | None] = None
+
+    def serialize(self, value: list[T]) -> bytes:
+        from pythonium.engine.codecs.custom import VarIntCodec  # noqa: PLC0415
+
+        length_codec = self.__length_codec__ or VarIntCodec()
+        length_bytes = length_codec.serialize(len(value))
+        element_bytes = b"".join(
+            self.__element_codec__.serialize(item) for item in value
+        )
+        return length_bytes + element_bytes
+
+    def deserialize(self, data: bytes) -> Deserialized[list[T]]:
+        from pythonium.engine.codecs.custom import VarIntCodec  # noqa: PLC0415
+
+        length_codec = self.__length_codec__ or VarIntCodec()
+        length, offset = length_codec.deserialize(data)
+        result = []
+
+        for _ in range(length):
+            item, item_size = self.__element_codec__.deserialize(data[offset:])
+            result.append(item)
+            offset += item_size
+
+        return result, offset
 
 
 class PrimitiveCodec[T](Codec[T]):
