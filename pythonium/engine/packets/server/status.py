0e6fdc5dea631e79b8480ffde4afd91d6a4e3da5
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-17 23:53:15 +0300

    refactor: update server status packet

--- pythonium/engine/packets/server/status.py
+++ pythonium/engine/packets/server/status.py
@@ -1,19 +1,54 @@
-from typing import ClassVar
+from typing import ClassVar, TypedDict
 
 from pythonium.engine.enums import Direction, State
 from pythonium.engine.packets import Packet
-from pythonium.engine.types import Long, String, VarInt
+from pythonium.engine.types import Long, VarInt
+
+
+class VersionDict(TypedDict):
+    """Dictionary for version information."""
+
+    name: str
+    protocol: int
+
+
+class SamplePlayersDict(TypedDict):
+    """Dictionary for sample player information."""
+
+    name: str
+    id: str
+
+
+class PlayersDict(TypedDict):
+    """Dictionary for player information."""
+
+    max: int
+    online: int
+    sample: list[SamplePlayersDict]
+
+
+class DescriptionDict(TypedDict):
+    """Description dictionary for server status."""
+
+    text: str
 
 
 class ServerStatus(Packet, kw_only=True):
     """Packet representing status."""
+
+    __schema_as_json__ = True
 
     __state__ = State.STATUS
     __direction__ = Direction.CLIENTBOUND
 
     packet_id: ClassVar[VarInt] = 0x00
 
-    json_response: String
+    version: VersionDict
+    players: PlayersDict
+    description: DescriptionDict
+
+    favicon: str = "data:image/png;base64,<data>"
+    enforcesSecureChat: bool = False  # noqa: N815
 
 
 class Pong(Packet, kw_only=True):
