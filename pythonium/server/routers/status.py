251e613651ec43ce9a48ea5155d19400116a7905
Ivan Pythonov <bsbedhs@gmail.com>
2026-01-17 23:53:16 +0300

    refactor: update status router implementation

--- pythonium/server/routers/status.py
+++ pythonium/server/routers/status.py
@@ -1,7 +1,5 @@
 """Some logic for router."""
 
-from json import dumps
-
 from pythonium.engine import Router
 from pythonium.engine.packets import GetStatus, Ping, Pong, ServerStatus
 
@@ -11,24 +9,18 @@ router = Router(name=__name__)
 @router.on(GetStatus)
 async def on_status(_status: GetStatus) -> ServerStatus:
     return ServerStatus(
-        json_response=dumps(
-            {
-                "version": {"name": "1.21.8", "protocol": 772},
-                "players": {
-                    "max": 100,
-                    "online": 1,
-                    "sample": [
-                        {
-                            "name": "thanks_wiki_vg",
-                            "id": "4566e69f-c907-48ee-8d71-d7ba5aa00d20",
-                        }
-                    ],
-                },
-                "description": {"text": "Hello, world!"},
-                "favicon": "data:image/png;base64,<data>",
-                "enforcesSecureChat": False,
-            }
-        )
+        version={"name": "1.21.8", "protocol": 772},
+        players={
+            "max": 100,
+            "online": 1,
+            "sample": [
+                {
+                    "name": "thanks_wiki_vg",
+                    "id": "4566e69f-c907-48ee-8d71-d7ba5aa00d20",
+                }
+            ],
+        },
+        description={"text": "Hello, world!"},
     )
 
 
